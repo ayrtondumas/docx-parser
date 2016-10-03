2350370f0c34e4635c137e9d3fe00c3a775982b8
Ayrton Dumas <ayrtondumas@gmail.com>
2016-10-03 09:15:25 +0200

    init git

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# docx-parser
+Docx-parser
+===================
+
+
+A simple module to get the plain text from a docx file.
+
+----------
+
+**exemple**
+```
+var docxParser = require('docx-parser');
+
+docxParser.parseDocx("example.docx", function(data){
+        console.log(data)
+})
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+const decompress = require('decompress');
+
+var parseDocx = function(filename, callback){
+  decompress(filename, 'dist').then(files => {
+    fs = require('fs')
+    fs.readFile('dist/word/document.xml', 'utf8', function (err,data) {
+	     if (err) {
+   	     return console.log(err);
+ 	     }
+
+    	var plain_text = data.replace(/(<([^>]+)>)/ig, "");
+    	callback(plain_text);
+    });
+  });
+}
+
+
+module.exports.parseDocx = parseDocx;
